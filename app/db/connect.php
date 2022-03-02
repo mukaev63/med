a86de8b23dd70386fab31768eaaee0577cdaf990
mukaev63 <62661332+mukaev63@users.noreply.github.com>
2022-03-03 00:18:37 +0600

    Update connect.php

--- app/db/connect.php
+++ app/db/connect.php
@@ -1,10 +1,10 @@
 <?php
 
-$driver = 'mysql';
-$host = 'localhost';
-$db_name = 'med';
-$db_user = 'root';
-$db_pass = '';
+$driver = 'postgresql';
+$host = 'ec2-54-83-21-198.compute-1.amazonaws.com';
+$db_name = 'd1e96jcbqukpke';
+$db_user = 'eabzomaiwwtqsv';
+$db_pass = '1d5850829323a5c4319d1cc7bdcbd9f219a95f43a192f66de957f0b7ddbd7910';
 $charset = 'utf8';
 $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
